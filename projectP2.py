082aad08b0865d2078a29fa5f49d983923f2860b
TomasUrena <142543789+TomasUrena@users.noreply.github.com>
2024-10-11 10:10:46 -0600

    Create projectP2.py

---
+++ projectP2.py
@@ -0,0 +1,27 @@
+# Tomas Urena 
+# 10/10/2024
+# Project Part 2
+
+import mysql.connector
+import sys
+
+x = sys.argv[1]
+match x:
+    case "1":
+        print()
+    case "2":
+        print()
+    case "3":
+        print()
+    case "4":
+        print()
+    case "5":
+        print()
+    case "6":
+        print()
+    case "7": 
+        print()
+    case "8":
+        print()
+        
+print('Hello') 
